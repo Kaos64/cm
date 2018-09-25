1d2b60f580499bb89cbbcc47bad22efe383669d9
Kaos <appssa12>
2018-09-25 08:44:10 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './screen/main.component';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatInputModule, MatFormFieldModule, MatGridListModule, MatCardModule, MatPaginatorModule } from '@angular/material';
+
+import { RouterModule } from '@angular/router';
+import { LayoutModule } from '@angular/cdk/layout';
+import { AppRoutingModule } from './app-routing.module';
+import { SearchComponent } from './screen/search/search.component';
+import { QueryComponent } from './screen/search/query.component';
+import { ResultComponent } from './screen/search/result.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ExploreComponent } from './screen/search/explore.component';
+import { AlbumComponent } from './screen/search/album.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SearchComponent,
+    QueryComponent,
+    ResultComponent,
+    ExploreComponent,
+    AlbumComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatPaginatorModule,
+    FlexLayoutModule,
+
+
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
